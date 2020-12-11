9fa9ad820542d44b3c6c45f76ed64b0902d91d0e
charles <j95.charles@gmail.com>
2020-12-10 22:11:38 -0500

    day9

---
+++ day9/addition.py
@@ -0,0 +1,31 @@
+import sys
+from itertools import combinations
+
+with open(sys.argv[1]) as nums_fle:
+    nums = list(map(int, nums_fle.read().split()))
+
+special_num = None
+step = 25
+for i, num in enumerate(nums[25:]):
+    sum_nums = map(sum, combinations(nums[i:step], 2))
+    step += 1
+    if num not in sum_nums:
+        special_num = num
+        break
+    
+def group(lst, start, size):
+    return lst[start : start + size]
+
+min_max_sum = None
+for start in range(0, len(nums)):
+    for size in range(0, len(nums)):
+        groups = sum(group(nums, start, size))
+
+        if groups == special_num:
+            slce = nums[start:start+size]
+            min_max_sum = min(slce) + max(slce)
+            break
+
+
+print(f"Part one is: {special_num}")
+print(f"Part two is: {min_max_sum}")
